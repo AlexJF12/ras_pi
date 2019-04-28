66e75f641cd32058048efec5486026bf38454763
Alex Freeman <alexjf12@gmail.com>
2019-04-28 19:03:04 -0400

    fix

--- camera/make_gif.py
+++ camera/make_gif.py
@@ -9,14 +9,14 @@ system('rm -rf gif_images/*')
 camera = PiCamera()
 
 for i in range(10):
-    camera.capture('image{0:04d}.jpg'.format(i))
+    camera.capture('gif_images/image{0:04d}.jpg'.format(i))
 
 #set timestamp str
 ts_str = str(dt.datetime.now().strftime("%Y_%m_%d-%H_%M_%S"))
 
 #set path to save gif
-path_str = '/home/pi/Pictures/gif_{}.jpg'.format(ts_str)
+path_str = '/home/pi/Pictures/gif_{}.gif'.format(ts_str)
 
 # convert gif
-system('convert -delay 10 -loop 0 image*.jpg {}'.format(path_str))
+system('convert -delay 10 -loop 0 gif_images/image*.jpg {}'.format(path_str))
 print('done')
