2dd301d8c091215601a0945f64ad861842532b67
Alex Freeman <alexjf12@gmail.com>
2019-10-13 11:12:08 -0400

    add annotation

--- camera/take_pic_5_min.py
+++ camera/take_pic_5_min.py
@@ -12,7 +12,8 @@ ts_str = str(dt.datetime.now().strftime("%Y%m%d-%H%M%S"))
 #set path to save pic
 path_str = '/home/pi/Pictures/every_five_min/pic_{}.jpg'.format(ts_str)
 
-#take pic, save in rasberry_pi directory
+#take pic, annotate, save in rasberry_pi directory
+camera.annotate_text = ts_str
 camera.capture(path_str)
 
 # print('pic taken')
