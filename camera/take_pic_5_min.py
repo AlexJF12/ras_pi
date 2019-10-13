4ebfb76cae8696989df4b73681f20170fcf21182
Alex Freeman <alexjf12@gmail.com>
2019-10-13 11:57:18 -0400

    autoset wb

--- camera/take_pic_5_min.py
+++ camera/take_pic_5_min.py
@@ -2,18 +2,30 @@
 
 from picamera import PiCamera
 import datetime as dt
+import time
 
-# init camera
-camera = PiCamera()
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280, 720)
+    camera.framerate = 30
+    # Wait for the automatic gain control to settle
+    time.sleep(3)
+    # Now fix the values
+    camera.shutter_speed = camera.exposure_speed
+    camera.exposure_mode = 'off'
+    g = camera.awb_gains
+    camera.awb_mode = 'off'
+    camera.awb_gains = g
 
-#set timestamp str
-ts_str = str(dt.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    camera.annotate_size = 24
+    camera.annotate_foreground = Color('black')
+    camera.annotate_background = Color('white')
+    camera.annotate_text = str(dt.datetime.now().strftime("%Y-%m-%d-%H:M:S"))
 
-#set path to save pic
-path_str = '/home/pi/Pictures/every_five_min/pic_{}.jpg'.format(ts_str)
+    #set timestamp str
+    ts_str = str(dt.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
-#take pic, annotate, save in rasberry_pi directory
-camera.annotate_text = ts_str
-camera.capture(path_str)
+    #set path to save pic
+    path_str = '/home/pi/Pictures/every_five_min/pic_{}.jpg'.format(ts_str)
 
-# print('pic taken')
+    # take pic
+    camera.capture(path_str)
