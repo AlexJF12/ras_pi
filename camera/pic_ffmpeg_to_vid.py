0b4dcb29d6b45c1e0f6c3fc23199f33cf8090b3e
Alex Freeman <alexjf12@gmail.com>
2019-11-09 17:08:10 -0500

    cli pic to vid

---
+++ camera/pic_ffmpeg_to_vid.py
@@ -0,0 +1,11 @@
+# pic_ffmpeg_to_vid.py
+
+# command line to convert yesterdays pics to vid
+
+import os
+
+date_for_vid = str((dt.datetime.now() - dt.timedelta(days=1)).strftime("%Y%m%d"))
+
+command_line_code = "ffmpeg -framerate 10 -f image2 -pattern_type glob -i 'pic_" + date_for_vid + "-*.jpg' ~/daily_vids/" + date_for_vid + "}.mp4"
+
+os.system()
