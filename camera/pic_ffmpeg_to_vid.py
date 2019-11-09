7a2f86a2971dadf089f9dbd70c29c0c9bd4f9422
Alex Freeman <alexjf12@gmail.com>
2019-11-09 17:19:20 -0500

    fdva

--- camera/pic_ffmpeg_to_vid.py
+++ camera/pic_ffmpeg_to_vid.py
@@ -9,7 +9,7 @@ date_for_vid = str((dt.datetime.now() - dt.timedelta(days=1)).strftime("%Y%m%d")
 
 # ffmpeg -framerate 10 -f image2 -pattern_type glob -i 'pic_20191108-*.jpg' ~/Documents/test.mp4
 
-command_line_code = "ffmpeg -framerate 10 -f image2 -pattern_type glob -i '~/Pictures/every_five_min/pic_" + date_for_vid + "-*.jpg' ~/Documents/daily_vids/" + date_for_vid + ".mp4"
+command_line_code = "cd ~/Pictures/every_five_min ; ffmpeg -framerate 10 -f image2 -pattern_type glob -i 'pic_" + date_for_vid + "-*.jpg' ~/Documents/daily_vids/" + date_for_vid + "_vid.mp4"
 
 print(command_line_code)
 
