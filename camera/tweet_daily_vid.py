3d9e08ee9a4d1108ade455048b0beee4767a5e69
Alex Freeman <alexjf12@gmail.com>
2019-11-10 10:55:54 -0500

    update

--- camera/tweet_daily_vid.py
+++ camera/tweet_daily_vid.py
@@ -19,10 +19,14 @@ twitter = Twython(
 
 date_for_vid = str((dt.datetime.now() - dt.timedelta(days=1)).strftime("%Y%m%d"))
 
-message = date_for_vid + " in 30 seconds"
-video = open('~/Documents/daily_vids/' + date_for_vid + '_vid.mp4', 'rb')
+file_name = '~/Documents/daily_vids/' + date_for_vid + '_vid.mp4'
+print(file_name)
+
+video = open(file_name, 'rb')
 
 response = twitter.upload_video(media=video, media_type='video/mp4')
+
+message = date_for_vid + " in 30 seconds"
 
 twitter.update_status(status=message, media_ids=[response['media_id']])
 
